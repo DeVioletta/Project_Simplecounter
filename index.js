789266345b80ce2d5254c55abe76a274b3c057c5
DeVioletta <senjougahara.violet@gmail.com>
2024-08-23 10:23:24 +0700

    add localstorage to save

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
-let count = 0
-let numberEl = document.getElementById("number")
-let saveEl = document.getElementById("save-el")
-let totalEL = document.getElementById("total-el")
-let total = 0
+let count;
+const numberEl = document.getElementById("number");
+const saveEl = document.getElementById("save-el");
+const totalEL = document.getElementById("total-el");
+let total;
+let countArr = [];
 
-function increment(){
-    count = count + 1
-    numberEl.textContent = count
+const minusBtn = document.getElementById("minus-btn");
+minusBtn.addEventListener("click", minus);
+
+const incrementBtn = document.getElementById("increment-btn");
+incrementBtn.addEventListener("click", increment);
+
+const saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", save);
+
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", reset);
+
+const storedCount = JSON.parse(localStorage.getItem("count"));
+const storedTotal = parseInt(localStorage.getItem("total"), 10);
+
+if (storedCount && storedTotal) {
+    countArr = storedCount;
+    total = storedTotal;
+    renderCount();
 }
 
-function minus(){
-    count = count - 1
-    numberEl.textContent = count
+count = 0;
+
+function increment() {
+    count = count + 1;
+    numberEl.textContent = count;
 }
 
-function save(){
-    saveEl.textContent += count + ", "
-    total += count
-    count=0
-    numberEl.textContent = count
-    totalEL.textContent = "Total : " + total
+function minus() {
+    count = count - 1;
+    numberEl.textContent = count;
 }
 
-function reset(){
-    total = 0
-    totalEL.textContent = "Total : "
-    saveEl.textContent = "Reminder : "
+function save() {
+    countArr.push(count);
+    
+    count = 0;
+    numberEl.textContent = count;
+    renderCount();
+
+    localStorage.setItem("count", JSON.stringify(countArr));
+    localStorage.setItem("total", total.toString());
+}
+
+function renderCount() {
+    total = 0;
+    let countText = "";
+    for (i = 0; i < countArr.length; i++) {
+        countText += `${countArr[i]},`;
+        total += countArr[i];
+        console.log("save ~ countText:", countText);
+    }
+    console.log(countArr);
+
+    saveEl.textContent = "Reminder : " + countText;
+    totalEL.textContent = "Total : " + total;
+}
+
+function reset() {
+    localStorage.clear();
+    countArr = [];
+    totalEL.textContent = "Total : ";
+    saveEl.textContent = "Reminder : ";
 }
